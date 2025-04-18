bf755629bad4beea30cd35648606317ba307e01f
duongnguyenhocit <duongdtth17jtt@gmail.com>
2025-04-18 22:13:09 +0700

    add new code all

--- NguyenDinhDuong_21132261_Tuan6/MicroserviceProduct/customer-service/src/main/java/iuh/fit/se/customerservice/services/CustomerService.java
+++
@@ -1,8 +0,0 @@
-package iuh.fit.se.customerservice.services;
-
-import iuh.fit.se.customerservice.entities.Customer;
-
-public interface CustomerService {
-    public Customer save(Customer customer);
-    public Customer findById(long id);
-}
--- NguyenDinhDuong_21132261_Tuan6/MicroserviceProduct/order-service/src/main/java/iuh/fit/se/orderservice/services/OrderService.java
+++
@@ -1,8 +0,0 @@
-package iuh.fit.se.orderservice.services;
-
-import iuh.fit.se.orderservice.entities.Order;
-
-public interface OrderService {
-    public Order save(Order order);
-    public Order findById(long id);
-}
